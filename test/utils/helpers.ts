c70183a53dd04b2c48b00234516b423cbaf5e028
Asher <ash@coder.com>
2021-06-17 17:36:44 -0500

    Cache options parsing
    This way it can be called multiple times without incurring extra
    overhead or potentially resolving in a different way.
    
    Also catch any errors when parsing query options just as we do with the
    options in the markup and log errors with the latter as well.

--- test/utils/helpers.ts
+++ test/utils/helpers.ts
@@ -20,6 +20,18 @@ export function createLoggerMock() {
 }
 
 /**
+ * Spy on console and surpress its output.
+ */
+export function spyOnConsole() {
+  const noop = () => undefined
+  jest.spyOn(global.console, "debug").mockImplementation(noop)
+  jest.spyOn(global.console, "error").mockImplementation(noop)
+  jest.spyOn(global.console, "info").mockImplementation(noop)
+  jest.spyOn(global.console, "trace").mockImplementation(noop)
+  jest.spyOn(global.console, "warn").mockImplementation(noop)
+}
+
+/**
  * Create a uniquely named temporary directory.
  *
  * These directories are placed under a single temporary code-server directory.
